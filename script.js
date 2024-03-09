0aaf08ed52566dec84225e5ce2f921dfc6ea930d
Paulette <paulettehm0421@gmail.com>
2024-03-09 15:33:00 -0700

    Optimized Pictures

--- script.js
+++ script.js
@@ -14,5 +14,12 @@ function activateButton(clickedElement) {
     clickedElement.classList.add('active');
   }
 
+  window.addEventListener('scroll', function() {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
+
 
   document.getElementById("currentYear").textContent = new Date().getFullYear();
