eac665f25a86113841e31f3bed0ed171fd362aa4
Paulette <paulettehm0421@gmail.com>
2024-03-09 22:03:21 -0700

    icon included

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
+
+// Scroll Effects
 AOS.init({
     duration: 1250,
   })
 
-
+// Small script for the menu bar
+// It shows a border bottom line when the button is active
 function activateButton(clickedElement) {
-    // Remove 'active' class from all links
+
     var links = document.querySelectorAll('.nav-link');
     links.forEach(function(link) {
       link.classList.remove('active');
     });
 
-    // Add 'active' class to the clicked link
+
     clickedElement.classList.add('active');
   }
 
-  window.addEventListener('scroll', function() {
-    const parallax = document.querySelector('.parallax');
-    let scrollPosition = window.pageYOffset;
 
-    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-});
 
 
   document.getElementById("currentYear").textContent = new Date().getFullYear();
